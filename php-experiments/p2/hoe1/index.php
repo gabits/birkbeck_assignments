12fe181611871ab9963650deaa42a6d13011e1b9
Gabriela Valle <gabrieladvmininel@gmail.com>
2018-11-10 23:49:45 +0000

    Add exercises with arrays

---
+++ php-experiments/p2/hoe1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+
+    <body>
+        <?php
+            $lett = array(2, 3, 4,1);
+
+            echo "<pre>";
+            print_r($lett);
+            echo "</pre>";
+        ?>
+    </body>
+</html>
