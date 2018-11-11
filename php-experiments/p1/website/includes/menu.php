e4bda9a61c8d4a73d4c7f8040f5b623422c5a3f6
Gabriela Valle <gabrieladvmininel@gmail.com>
2018-11-11 20:50:30 +0000

    Make website menu static

--- php-experiments/p1/website/includes/menu.php
+++ php-experiments/p1/website/includes/menu.php
@@ -1,3 +1,12 @@
 <?php
-
+    $menu = array(
+        'index.php' => 'Home',
+        'people.php' => 'People',
+        'weather.php' => 'Weather',
+    );
+    echo '<ul>';
+    foreach($menu as $file => $title) {
+        echo "<li><a href=$file>$title</a></li>";
+    }
+    echo '</ul>';
 ?>
--- php-experiments/p1/website/weather.php
+++ php-experiments/p1/website/weather.php
@@ -7,25 +7,23 @@
 </head>
 <body>
 	<div id="page">
-        
+
         <h1><a href="index.php">P1</a></h1>
-		
+
 		<div class="nav">
-			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="people.php">People</a></li>
-				<li><a href="weather.php">Weather</a></li>
-			</ul>
+            <?php
+                include 'includes/menu.php';
+            ?>
 		</div>
-		
+
         <h2>Weather</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
-        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
-        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
         mollit anim id est laborum.</p>
-        
+
 		<div id="footer">
 			<p>Web Programming using PHP</p>
 		</div>
